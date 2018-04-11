72ef9f6ea2ec9db0b8e2b13d3b0100c828d6f4c6
christianb93 <christian_bohr@web.de>
2018-04-11 19:54:21 +0200

    Added alternative option to download the MNIST data set if there is a problem with mldata.org

--- RBM.py
+++ RBM.py
@@ -114,7 +114,27 @@ class TrainingData:
         elif ds == "MNIST":
             if (N != 28):
                 raise ValueError("Please use N = 28 for the MNIST data set")
-            mnist = fetch_mldata("MNIST original")
+            try:
+                mnist = fetch_mldata('MNIST original')
+            except urllib.error.HTTPError as ex:
+                print("Could not download MNIST data from mldata.org, trying alternative...")
+
+                # Alternative method to load MNIST, if mldata.org is down 
+                from scipy.io import loadmat
+                mnist_alternative_url = "https://github.com/amplab/datascience-sp14/raw/master/lab7/mldata/mnist-original.mat"
+                mnist_path = "./mnist-original.mat"
+                response = urllib.request.urlopen(mnist_alternative_url)
+                with open(mnist_path, "wb") as f:
+                    content = response.read()
+                    f.write(content)
+                mnist_raw = loadmat(mnist_path)
+                mnist = {
+                    "data": mnist_raw["data"].T,
+                    "target": mnist_raw["label"][0],
+                    "COL_NAMES": ["label", "data"],
+                "DESCR": "mldata.org dataset: mnist-original",
+                }
+                print("Success!")
             label = mnist['target']
             mnist = mnist.data
             mnist = ((mnist / 255.0) + 0.5).astype(int)
