f0c904f8940d60918f3a369358e8e4b437c32abc
christianb93 <me@somewhere>
2024-12-20 17:09:00 +0100

    Regenerate diagrams

--- doc/Thermodynamics/scripts/VanDerWaals.py
+++ doc/Thermodynamics/scripts/VanDerWaals.py
@@ -7,15 +7,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def norm(T,V, N, a=1, b=1):
-    return 27*b/(8*a) * T, V / (2*b*N) 
+#
+# Normalize temperature and volume, i.e. divide
+# by the temperature respective volume at the
+# critical point
+# Recall that
+# T_c = 8a / 27b
+# V_c = 3 bN
+#
+def norm(T,V, N, a = 1, b = 1):
+    return 27*b/(8*a) * T, V / (3*b*N) 
 
-def P(T,V, N, a=1, b=1):
-    return  N*T/(V-b*N) - a*N*N/(V*V) 
+#
+# Determine the pressure as function of
+# temperature, volume and particle number
+#
+def P(T,V, N, a = 1, b = 1):
+    return  N*T/(V-b*N) - a*N*N/V**2 
 
-def Pb(Tb, Vb, N, a=1, b=1):
-    return 8.0*Tb/(3.0*Vb-1) - 3.0/(Vb*Vb)
 
+#
+# Determine the reduced pressure in terms of
+# reduced volume and reduced temperature
+#
+def Pb(Tb, Vb, N, a = 1, b = 1):
+    return 8.0*Tb/(3.0*Vb-1) - 3.0/Vb**2
+
+#
+# Determine the part of the reduced Gibbs energy that does not
+# depend on the temperature in terms of reduced volume and temperature
+# i.e. this is the molar Gibbs energy divided by k_B T_c and dropping
+# the terms that do not depend on V
+#
+def Gb(Tb, Vb):
+    return - Tb * (np.log(3.0*Vb - 1.0) - 1.0/(3.0*Vb - 1)) - 9.0/(4.0*Vb)
+
+#
+# Given a certain combination of reduced
+# temperature and pressure, determine the
+# three possible values of the reduced volume
+#
 def solve_forVb(Pb, Tb):
     roots = np.roots([3*Pb, -Pb-8*Tb, 9,-3])
     if len(roots) != 3:
@@ -23,20 +54,12 @@ def solve_forVb(Pb, Tb):
         print("Only ", len(roots), "roots found")
     return np.sort(roots)
 
-def U(T,V,N, a=1, b=1):
-    return 1.5*N*T - a*N*N/V
 
-def A(T,V,N, a=1, b=1):
-    return -N*T*np.log((V-b*N)/N) - 1.5*N*T*np.log(T) - a*N*N/V
-
-def Gb(Tb,Vb):
-    return - Tb * (np.log(3.0*Vb - 1.0) - 1.0/(3.0*Vb - 1)) - 9.0/(4.0*Vb)
-
-
-def S(T,V,N, a=1, b=1):
-    return 1.5*N + N*(np.log((V-b*N)/N) + 1.5*np.log(T))
-
-
+#
+# Approximate derivatives as finite differences
+# for given values of x and y 
+# Here x and y are supposed to be arrays 
+#
 def compute_derivatives(x,y):
     results = []
     for i in range(len(x)-1):
@@ -45,11 +68,24 @@ def compute_derivatives(x,y):
     return results
 
 
+#
+# Given a reduced temperature T_b, find the
+# coexistence state, i.e. the pressure for which
+# the Gibbs energies of the liquid and gas phase 
+# are equal. Thus we
+# - search a certain range of pressure values P_b
+# - for each range, we determine the corresponding values for the volume
+# - for each of the volumes, we calculate the Gibbs energies
+# - we stop if the sign of the difference between the Gibbs energies of
+#   the gas phase and the liquid phase changes
+#
 def find_coexistence_state(_Tb = 0.85):
     diff = 0.1
     for _Pb in np.arange(0.01, 1.0, 0.002):
-        _Vb1, _Vb2, _Vb3 = solve_forVb(Pb=_Pb,Tb=_Tb)
-        _Gbvalues = Gb(Tb=_Tb, Vb=np.array([_Vb1, _Vb2, _Vb3]))
+        _Vb1, _Vb2, _Vb3 = solve_forVb(Pb = _Pb,Tb = _Tb)
+        _Gbvalues = Gb(Tb = _Tb, Vb = np.array([_Vb1, _Vb2, _Vb3]))
+        # Here we assume that the middle value of the volume is the
+        # one in the unstable region
         diff_new = _Gbvalues[0] - _Gbvalues[2]
         if ((diff_new > 0) and (diff <= 0)) or ((diff_new <= 0) and (diff > 0)):
             return _Pb
@@ -68,13 +104,13 @@ print("Predicted for critical volume: ", 3*b)
 #
 # First we make a plot around the critical
 # region and show the dependency of the 
-# pressure from the volume
+# pressure as a function of the volume
 #
 
-fig = plt.figure(figsize=(10,8))
-ax0 = fig.add_subplot(2,2,1)
-ax0.set_xlabel("V")
-ax0.set_ylabel("P")
+fig = plt.figure(figsize = (10,8))
+ax0 = fig.add_subplot(2, 2, 1)
+ax0.set_xlabel("Volume V")
+ax0.set_ylabel("Pressure P")
 
 
 V = np.arange(0.19, 0.8, 0.001)
@@ -87,22 +123,24 @@ for T in np.arange(2.9, 3.4, 0.1):
 # Now we turn this around for a temperature
 # below the critical point
 #
-ax1 = fig.add_subplot(2,2,2)
+ax1 = fig.add_subplot(2, 2, 2)
 T = 2.9
-Tbar, _ = norm(T,1, N, a=a, b=b)
+Tbar, _ = norm(T,1, N, a = a, b = b)
 values = P(T,V,N, a = a, b = b)    
 ax1.plot(values, V, "g")
-ax1.set_xlabel("P")
-ax1.set_ylabel("V")
+ax1.set_xlabel("Pressure P")
+ax1.set_ylabel("Volume V")
 
 
 #
 # We now compute the derivative of P wrt to V
 #
 d = compute_derivatives(V, values)
-ax2 = fig.add_subplot(2,1,2)
+ax2 = fig.add_subplot(2, 1, 2)
 ax2.plot(V[1:], d, "g")
 ax2.plot(V[1:], np.zeros(len(V)-1), "y")
+ax2.set_xlabel("Volume V")
+ax2.set_ylabel("dP / dV")
 
 #
 # Mark the zeros in the picture above
@@ -112,7 +150,7 @@ for i in range(len(V)-2):
         ax1.plot(values[i+1], V[i+1], "rD")
         ax2.plot(V[i+1], 0, "rD")
 
-#plt.savefig("VanDerWaals.png")        
+plt.savefig("VanDerWaals.png")        
 
 #
 # Now we turn to the potentials. First we plot the 
@@ -120,13 +158,19 @@ for i in range(len(V)-2):
 # part of the P-V curve
 # 
 fig = plt.figure(figsize=(10,8))
-ax0 = fig.add_subplot(2,1,1, title="Gibbs energy")
+ax0 = fig.add_subplot(2,1,1)
 ax0.set_xlim(0, 6)
-ax1 = fig.add_subplot(2,1,2, title="Normalized pressure")
+ax0.set_xlabel("Normalized volume")
+ax0.set_ylabel("Normalized Gibbs energy g")
+
+ax1 = fig.add_subplot(2,1,2)
+ax1.set_xlabel("Normalized volume")
+ax1.set_ylabel("Normalized pressure")
 ax1.set_xlim(0, 6)
+
 Tb = 0.85
 Vb = np.arange(0.53, 6, 0.001)
-Pb = Pb(Tb, Vb, N, a=a, b=b)
+Pb = Pb(Tb, Vb, N, a = a, b = b)
 values = Gb(Tb,Vb)   
 ax0.plot(Vb,values)
 ax1.plot(Vb, Pb)
@@ -141,7 +185,7 @@ text = [["A","B","C"], ["D", "E", "F"], ["G", "H", "I"]]
 offset = 0.02
 i = 0
 for Pb in [0.505, 0.4]:
-    Vb1, Vb2, Vb3 = solve_forVb(Pb=Pb,Tb=0.85)
+    Vb1, Vb2, Vb3 = solve_forVb(Pb = Pb,Tb = 0.85)
     Gbvalues = Gb(Tb=0.85, Vb=np.array([Vb1, Vb2, Vb3]))
     line_y = np.full(600, Pb)
     ax1.plot(np.arange(0, 6, 0.01), line_y, lines[i])
@@ -153,7 +197,7 @@ for Pb in [0.505, 0.4]:
     ax1.text(Vb3, Pb + offset, text[2][i])
     ax0.plot(np.array([Vb1, Vb2, Vb3]), Gbvalues, markers[i])
     ax0.text(Vb1 - 0.2, Gbvalues[0] - offset*0.5, text[0][i])
-    # ax0.text(Vb2, Gbvalues[1]+ offset, text[1][i])
+    ax0.text(Vb2, Gbvalues[1]+ offset, text[1][i])
     ax0.text(Vb3, Gbvalues[2] + offset, text[2][i])
     ax0.plot(np.arange(0, 6, 0.01), np.full(600, Gbvalues[2]), lines[i])
     i += 1
